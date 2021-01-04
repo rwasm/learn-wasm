cc042e887d12b5b56759e7f0336df7d33e941be4
lencx <cxin1314@gmail.com>
2021-01-05 00:48:07 +0800

    chore: deploy

---
+++ vite.config.deploy.ts
@@ -0,0 +1,21 @@
+import * as reactPlugin from 'vite-plugin-react';
+import type { UserConfig } from 'vite';
+import path from 'path';
+
+const config: UserConfig = {
+  jsx: 'react',
+  minify: 'esbuild',
+  // write: true,
+  plugins: [
+    reactPlugin,
+  ],
+  // see: https://github.com/vitejs/vite/blob/master/src/node/config.ts
+  // the key must start and end with a slash
+  alias: {
+    '/@/': path.resolve(__dirname, 'src'),
+    '/hooks/': path.resolve(__dirname, 'src/hooks'),
+    'rust_crate': path.resolve(__dirname, 'rust-crate/pkg/rust_crate.js'),
+  },
+};
+
+export default config;
