baff215abc5bdf57917c9558abc548219a6c8876
lencx <cxin1314@gmail.com>
2021-01-10 02:45:04 +0800

    fix: wasm file loss

--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,7 @@ import { RouteOption } from '/router/types';
 const routes: RouteOption[] = [
   {
     path: '/',
-    component: lazy(() => import('./views/home')),
+    component: lazy(() => import('./views/Home')),
   }
   // {
   //   path: '/',
