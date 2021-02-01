6d3e9d5f9290d0a89de1a6782b3de0f34e574830
lencx <cxin1314@gmail.com>
2021-02-01 10:45:21 +0800

    chore: hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Router from './router/Router';
 
 import routes from './routes';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Router routes={routes} />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
