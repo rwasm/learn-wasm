d6310ff6f89970643f4f936810bf7d718f06ca36
lencx <cxin1314@gmail.com>
2021-04-03 19:56:38 +0800

    chore: upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
 import ViteRsw from 'vite-plugin-rsw';
 
-// global is not defined
-// Node stream polyfill not working: https://github.com/vitejs/vite/issues/1915
-
 export default defineConfig({
   plugins: [
     reactRefresh(),
