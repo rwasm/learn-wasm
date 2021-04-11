64ab6cfe4ce25981280e6153ff1604dd10dfe469
lencx <cxin1314@gmail.com>
2021-04-11 11:53:01 +0800

    chore: add awesome-rsw

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App';
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../routes';
+import awesomeRswIcon from '../assets/awesome-rsw.svg';
 
 export default function HomePage() {
   return (
@@ -22,9 +23,15 @@ export default function HomePage() {
       </div> */}
 
       <footer>
-        <a href="https://github.com/lencx/learn-wasm">lencx/learn-wasm</a>
-        {' 💖 '}
-        <a href="https://github.com/lencx/vite-plugin-rsw">rsw</a>
+        <div className="icons">
+          {/* <img src="https://img.shields.io/github/stars/lencx/learn-wasm?label=stars%28learn-wasm%29" /> */}
+          <a href="https://github.com/lencx/awesome-rsw"><img src={awesomeRswIcon} /></a>
+        </div>
+        <div className="f1">
+          <a href="https://github.com/lencx/learn-wasm">lencx/learn-wasm</a>
+          {' 💖 '}
+          <a href="https://github.com/lencx/vite-plugin-rsw">rsw</a>
+        </div>
       </footer>
     </div>
   )
