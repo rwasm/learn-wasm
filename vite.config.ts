22ab3609ca7c7da6e475bd5a31756359ee0590f2
lencx <cxin1314@gmail.com>
2022-02-27 22:38:30 +0800

    chore: rsw

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite';
 import ViteRsw from 'vite-plugin-rsw';
 
 export default defineConfig({
   plugins: [
-    reactRefresh(),
+    react(),
     ViteRsw(),
   ],
 })
